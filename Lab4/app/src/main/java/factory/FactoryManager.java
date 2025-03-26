b614cf7e93c2f25c02b577b8b113bb73f1a7ef84
ilyavolodinNSU <i.volodin@g.nsu.ru>
2025-03-27 02:47:21 +0700

    add hibernate

--- Lab4/app/src/main/java/factory/FactoryManager.java
+++ Lab4/app/src/main/java/factory/FactoryManager.java
@@ -1,45 +1,19 @@
 package factory;
 
-import factory.core.repository.IAccessoryRepository;
-import factory.core.repository.IBodyRepository;
-import factory.core.repository.ICarRepository;
-import factory.core.repository.IMotorRepository;
-import factory.core.services.AccessoryServices;
-import factory.core.services.BodyServices;
-import factory.core.services.CarServices;
-import factory.core.services.MotorServices;
-import factory.infrastructure.controllers.Dealer;
-import factory.infrastructure.controllers.SupplierAccessory;
-import factory.infrastructure.controllers.SupplierBody;
-import factory.infrastructure.controllers.SupplierMotor;
-import factory.infrastructure.controllers.Worker;
-import factory.infrastructure.db.repositories.AccessoryRepositoryStorage;
-import factory.infrastructure.db.repositories.BodyRepositoryStorage;
-import factory.infrastructure.db.repositories.CarRepositoryStorage;
-import factory.infrastructure.db.repositories.MotorRepositoryStorage;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import factory.infrastructure.db.entities.AccessoryData;
+import factory.infrastructure.db.entities.CarData;
+import factory.infrastructure.db.orm.HibernateUtil;
 
 public class FactoryManager {
-    //private IMotorRepository rep = new MotorRepositorySQL(), MotorRepositoryMongoDB(), etc...
+    public static void main(String[] args) {
+        
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession(); 
 
-    // repositories
-    private IMotorRepository motorRep = new MotorRepositoryStorage();
-    private IBodyRepository bodyRep = new BodyRepositoryStorage();
-    private IAccessoryRepository accessoryRep = new AccessoryRepositoryStorage();
-    private ICarRepository carRep = new CarRepositoryStorage();
-
-    // services
-    CarServices cs = new CarServices(carRep);
-    MotorServices ms = new MotorServices(motorRep);
-    BodyServices bs = new BodyServices(bodyRep);
-    AccessoryServices as = new AccessoryServices(accessoryRep);
-
-    // controllers
-    private SupplierMotor sm = new SupplierMotor(ms);
-    private SupplierBody sb = new SupplierBody(bs);
-    private SupplierAccessory sa = new SupplierAccessory(as);
-    private Worker w = new Worker(ms, bs, as, cs);
-    private Dealer d = new Dealer(cs);
-
-
-
+        CarData carData = session.get(CarData.class, 0);
+        System.out.println("Найден аксессуар: ID = " + carData.getId());
+    }
 }
---
+++ Lab4/app/src/main/java/factory/infrastructure/db/repositories/MotorRepositoryMySQL.java
@@ -0,0 +1,19 @@
+// package factory.infrastructure.db.repositories;
+
+// import org.hibernate.Session;
+
+// import factory.core.entities.parts.Motor;
+// import factory.core.repository.IMotorRepository;
+
+// public class MotorRepositoryMySQL implements IMotorRepository {
+
+//     @Override
+//     public void push(Motor car) {
+
+//     }
+
+//     @Override
+//     public Motor pop() {
+
+//     }
+// }
